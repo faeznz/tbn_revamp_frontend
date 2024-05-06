f1d5d3cfe8c7bcd6c789ef587d3da2dfcb5f5c58
Zulfan Faizun Najib <121880142+faeznz@users.noreply.github.com>
2024-05-06 13:05:31 +0700

    homepage update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from './page/home_page';
+import Tes from './page/tes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />}>
+          <Route index element={<Tes />} />
+          {/* <Route path="blogs" element={<Tes />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
