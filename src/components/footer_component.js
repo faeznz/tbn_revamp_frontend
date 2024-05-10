e2235b4ddc87d441213c61658024c9b7504b27c9
Krisna Harsya Saputra <krisna2100016032@webmail.uad.ac.id>
2024-05-10 18:19:35 +0700

    Update footer_components.js
    Mengecilkan footer

--- src/components/footer_component.js
+++ src/components/footer_component.js
@@ -1,17 +1,12 @@
-import React from "react";
+import React from 'react';
 
 const footer_component = () => {
   return (
     <div>
       <section className="bg-[#092040] w-full h-full flex flex-col rounded-t-[100px]">
-        <div className="flex flex-col items-center justify-center px-24 py-24">
-          <p className="text-white font-light text-xl mb-4">
-            email: hello@tbnalliance.org
-          </p>
-          <p className="text-white font-light text-xl ">
-            Copyright © 2024 - Transformational Business Network (TBN) Alliance
-            - All rights reserved.{" "}
-          </p>
+        <div className="flex flex-col items-center justify-center px-8 py-12">
+          <p className="text-white font-light text-sm mb-4">email: hello@tbnalliance.org</p>
+          <p className="text-white font-light text-sm ">Copyright © 2024 - Transformational Business Network (TBN) Alliance - All rights reserved. </p>
         </div>
       </section>
     </div>
