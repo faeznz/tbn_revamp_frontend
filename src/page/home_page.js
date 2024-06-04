f5d138bb73b0200c26e781210bb919693748a740
Zulfan Faizun Najib <121880142+faeznz@users.noreply.github.com>
2024-06-04 17:06:59 +0700

    feat: create env function

--- src/page/home_page.js
+++ src/page/home_page.js
@@ -32,6 +32,7 @@ function HomePage() {
     }
     return null;
   };
+  
 
   const getYoutubeEmbedUrl = (url) => {
     const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
