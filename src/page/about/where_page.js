6072726864c793f586ca5878606dc02de13db521
Krisna Harsya Saputra <krisna2100016032@webmail.uad.ac.id>
2024-06-10 20:15:39 +0700

    minor fix

--- src/page/about/where_page.js
+++ src/page/about/where_page.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
-import NavbarComponent from "../../components/navbar_component";
-import FooterComponent from "../../components/footer_component";
+import NavbarComponent from '../../components/navbar_component';
+import FooterComponent from '../../components/footer_component';
 
-import { FaWhatsapp } from "react-icons/fa";
-import { TbWorld } from "react-icons/tb";
+import { FaWhatsapp } from 'react-icons/fa';
+import { TbWorld } from 'react-icons/tb';
 
 import LippoThamrin from '../../assets/images/about/where-we-are/lippo-thamrin.jpeg';
 
@@ -15,23 +15,22 @@ const WherePage = () => {
       {/* Section 1 - Header */}
       <section>
         <div className="flex flex-col justify-center items-center w-screen lg:h-64 h-40 bg-[#C3D21F] lg:rounded-b-[100px] rounded-b-[30px]">
-          <p className="text-black lg:text-4xl text-2xl font-semibold pt-16">
-            Where We Are
-          </p>
+          <p className="text-black lg:text-4xl text-2xl font-semibold pt-16">Where We Are</p>
         </div>
       </section>
       {/* Section 2 - Description */}
-      <section className="pb-24">
-        <div className=" flex flex-col justify-center items-start lg:px-24 px-12 lg:pt-24 pt-12 text-white">
-          <div className="flex flex-row justify-between items-center gap-8 w-full bg-[#092040] rounded-2xl">
-            <div className="w-2/5 h-full">
-              <img src={LippoThamrin} alt="" className="rounded-l-2xl aspect-4/3 object-cover"/>
+      <section className="xl:pb-24 pb-12">
+        <div className=" flex flex-col justify-center items-start lg:px-24 xl:px-12 px-4 lg:pt-24 pt-12 text-white">
+          <div className="flex xl:flex-row flex-col justify-between items-center xl:gap-8 w-full bg-[#092040] rounded-2xl">
+            <div className="w-full h-full">
+              <img src={LippoThamrin} alt="" className="xl:rounded-l-2xl rounded-t-2xl aspect-4/3 object-cover" />
             </div>
-            <div className="pr-12">
-              <p className="lg:text-7xl text-lg text-[#C3D21F] font-bold">Reach Us On</p>
+            <div className="xl:pr-12 p-8">
+              <p className="lg:text-7xl text-2xl text-[#C3D21F] font-bold">Reach Us On</p>
               <p className="lg:text-xl text-lg font-light">Together, we can make a significant impact on the lives of Indonesians and the environment</p>
               <p className="lg:text-2xl text-lg font-semibold mt-16">Impact Hub</p>
-              <p className="lg:text-2xl text-lg font-light">Lippo Thamrin 5th Floor <br />
+              <p className="lg:text-2xl text-lg font-light">
+                Lippo Thamrin 5th Floor <br />
                 M.H, Thamrin No. 20 <br />
                 Central Jakarta, 10350, Indonesia
               </p>
